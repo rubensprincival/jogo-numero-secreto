56347a01c76235ee7c428979f8093bf8142af02c
RUBENS ALEX PRINCIVAL <155497390+rubensprincival@users.noreply.github.com>
2024-06-25 21:21:11 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+alert('Boas vindas ao jogo do número secreto');
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random()* numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+while(chute != numeroSecreto) {
+    chute = prompt(`Escolha um número de 1 a ${numeroMaximo}`);
+    if (numeroSecreto == chute) {
+        break;
+    } else {
+        if(numeroSecreto > chute) {
+            alert(`O número secreto é maior que ${chute}`);
+        } else{
+            alert(`O número secreto é menor que ${chute}`);
+        }
+    
+        tentativas++;
+    }
+}
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert (`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativas}. `);
+
